dd33cae1f8919e603657ce43506e9e21fa5d6f59
Md. Siam Shekh <siam62349@gmail.com>
2024-09-06 19:04:00 +0600

    a

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div className='text-3xl'>
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsa molestias iure sed odit similique, illum non quaerat accusantium blanditiia quod.
+      <div className="size-20 bg-white"></div>
+    </div>
+  );
+};
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,56 @@
+/** @type {import('tailwindcss').Config} */
+import daisyui from "daisyui"
+
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [daisyui],
+  daisyui: {
+    themes: [
+      "light",
+      "dark",
+      "cupcake",
+      "bumblebee",
+      "emerald",
+      "corporate",
+      "synthwave",
+      "retro",
+      "cyberpunk",
+      "valentine",
+      "halloween",
+      "garden",
+      "forest",
+      "aqua",
+      "lofi",
+      "pastel",
+      "fantasy",
+      "wireframe",
+      "black",
+      "luxury",
+      "dracula",
+      "cmyk",
+      "autumn",
+      "business",
+      "acid",
+      "lemonade",
+      "night",
+      "coffee",
+      "winter",
+      "dim",
+      "nord",
+      "sunset",
+    ],
+    darkTheme: "autumn", 
+    base: true, 
+    styled: true, 
+    utils: true,
+    prefix: "", 
+    logs: true, 
+    themeRoot: ":root", // 
+  },
+}

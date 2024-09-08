dd295c5360eaa9c937e17150c4d961bfb459a3ac
PlanqSiam <planqnetwork@gmail.com>
2024-09-08 10:47:33 +0600

    Login logic

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,17 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import { RouterProvider } from 'react-router-dom'
 import DomRoutes from './routes.tsx'
+import Context from './components/context/Context.tsx'
+import { Toaster } from 'react-hot-toast'
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <div data-theme="garden" >
-      <RouterProvider router={DomRoutes} />
+      <Context>
+        <RouterProvider router={DomRoutes} />
+        <Toaster />
+      </Context>
     </div>
   </React.StrictMode>,
 )
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,8 +1,23 @@
-import React from 'react';
 import { FaGoogle } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
+import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import toast from 'react-hot-toast';
+import auth from '../../firebase.config';
 
 const Login = () => {
+
+    const GoogleProvider = new GoogleAuthProvider();
+    const HandleGoogleLogin = () => {
+        const toastId = toast.loading('Request for login');
+        signInWithPopup(auth, GoogleProvider)
+            .then(() => {
+                toast.success('Requset Accpeted!', { id: toastId });
+            }).catch((error) => {
+                const errorMessage = error.message;
+                toast.error(errorMessage, { id: toastId });
+            });
+    }
+    // add more logic for login
     return (
         <div>
             <p className="font-bold text-4xl text-center">Welcome back!</p>
@@ -11,13 +26,13 @@ const Login = () => {
                 <input type="text" className="bg-gray-300 outline-none border border-white w-full h-10 rounded-xl px-5" placeholder="Password" />
                 <div className="divider">OR</div>
 
-                <button className="bg-white outline-none border border-white w-full h-10 rounded-full px-5 flex justify-center items-center gap-3 text-xl font-mono">
+                <button type='button' onClick={HandleGoogleLogin} className="bg-white outline-none border border-white w-full h-10 rounded-full px-5 flex justify-center items-center gap-3 text-xl font-mono">
                     Login via Google
                     <FaGoogle />
                 </button>
 
 
-                <button className="outline-none border border-white w-full h-10 rounded-full px-5 flex justify-center items-center gap-3 text-xl font-mono bg-primary">
+                <button type='submit' className="outline-none border border-white w-full h-10 rounded-full px-5 flex justify-center items-center gap-3 text-xl font-mono bg-primary">
                     Login
                 </button>
 

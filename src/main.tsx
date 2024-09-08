38a090e8d3923aa2ffeba8541e27efb6dbe1aa04
PlanqSiam <planqnetwork@gmail.com>
2024-09-08 14:52:00 +0600

    Firebase login removed [deleted]

--- src/main.tsx
+++ src/main.tsx
@@ -3,17 +3,14 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import { RouterProvider } from 'react-router-dom'
 import DomRoutes from './routes.tsx'
-import Context from './components/context/Context.tsx'
 import { Toaster } from 'react-hot-toast'
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <div data-theme="garden" >
-      <Context>
-        <RouterProvider router={DomRoutes} />
-        <Toaster />
-      </Context>
+      <RouterProvider router={DomRoutes} />
+      <Toaster />
     </div>
   </React.StrictMode>,
 )
